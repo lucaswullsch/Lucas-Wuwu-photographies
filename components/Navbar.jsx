97c910b6bffb3f7bacebac373f775dda9ff6d4ef
lucaswullsch <lucas.wullsch@gmail.com>
2025-11-27 10:44:58 +0100

    Add files via upload

---
+++ components/Navbar.jsx
@@ -0,0 +1,1 @@
+export default function Navbar(){return <header><div>Navbar</div></header>}
---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+module.exports = { content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'], darkMode: 'class', theme: { extend: {} }, plugins: [] }
